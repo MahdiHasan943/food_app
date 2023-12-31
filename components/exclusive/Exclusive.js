4a1924d3a6c3c9521840a6fe9e210e1ee07d0efd
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-12-31 12:10:39 +0600

    style:made exclusive slider

--- components/exclusive/Exclusive.js
+++ components/exclusive/Exclusive.js
@@ -1,9 +1,59 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import { exclusive } from "@/front_utils/info";
+import Image from "next/image";
 
 const Exclusive = () => {
-  return (
-    <div className='py-20 h-[400px] w-full px-4 desktop:px-16'>Exclusive</div>
-  )
-}
+  const swiperRef = useRef(null);
 
-export default Exclusive
+  const breakpoints = {
+    300: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 3,
+
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  };
+
+  return (
+    <div className='py-20 h-[400px] overflow-hidden w-full px-4 desktop:px-16'>
+      <Swiper
+        ref={swiperRef}
+        watchSlidesProgress={true}
+        slidesPerView={3}
+        className="mySwiper"
+        breakpoints={breakpoints}
+      >
+        {exclusive.map((content, index) => (
+          <SwiperSlide key={index}>
+            <div className="px-3 relative group ">
+              <Image
+                src={content.img}
+                alt={content.title}
+                width={400}
+                height={400}
+                className=" rounded-lg "
+              />
+              <p className="absolute top-0 right-[20px] bg-[#000] text-[#fff] px-4 rounded-md py-2">{content.offer}</p>
+              <div className={content.id===2? 'absolute bottom-0  duration-200 delay-75 ease-linear  left-0 px-8 py-4':` absolute bottom-[-100px] group-hover:bottom-0  duration-200 delay-75 ease-linear  left-0 px-8 py-4`}>
+              <p className="text-[#FC8A06]">{content.title}</p>
+              <h3 className="text-[#fff] text-[22px] leading-[25px] font-semibold">{content.des}</h3>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Exclusive;
--- front_utils/info.js
+++ front_utils/info.js
@@ -21,3 +21,35 @@ export const navlinks = [
         "href":"contact"
    }
 ]
+
+export const exclusive = [
+    {
+        "id": 1,
+        "title": "Restaurant",
+        "offer": "-40%",
+        "des": "Chef Burgers London",
+        "img":"/images/ex1.png"
+    },
+    {
+        "id": 2,
+        "title": "Restaurant",
+        "offer": "-17%",
+        "des": "Grand Ai Cafe London",
+        "img":"/images/ex2.png"
+    },
+    {
+        "id": 3,
+        "title": "Restaurant",
+        "offer": "-20%",
+        "des": "Butterbrot Caf’e London",
+        "img":"/images/ex3.png"
+    },
+    {
+        "id": 4,
+        "title": "Restaurant",
+        "offer": "-40%",
+        "des": "Chef Burgers London",
+        "img":"/images/ex1.png"
+    }
+    
+]
